d9381d1f9f25726c304f95749d8205b2805c458b
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-02-09 15:47:24 +0530

    Add files via upload

---
+++ 31_Random_Point_In_Non_Overlapping_Rectangles.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> rect;
+    int i=0;
+    int x,y;
+    Solution(vector<vector<int>>& rect) {
+        this->rect=rect;
+        x=rect[i][0];
+        y=rect[i][1];
+    }
+    
+    vector<int> pick() {
+        vector<int>ans={x,y};
+        x++;
+        if(x>rect[i][2]){
+            x=rect[i][0];
+            y++;
+        }
+        if(y>rect[i][3]){
+            if(i+1 < rect.size()){
+                i++;
+            }else{
+                i=0;
+            }
+            x=rect[i][0];
+            y=rect[i][1];
+        }
+        return ans;
+    }
+};
