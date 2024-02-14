393478c15060c30ab796205302c9a5e9bad2a387
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-02-14 22:25:04 +0530

    Add files via upload

---
+++ 47_Maximum_Product_Of_The_Length_Of_Two_Palindromic_Subsequences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    bool isPalin(string &s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j) {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    void dfs(string &s, int p, string &s1, string &s2, int &ans) {
+        if(p >= s.length()) {
+            if(isPalin(s1) && isPalin(s2)) {
+                int tmp = s1.length() * s2.length();
+                ans = max(ans, tmp);
+            }
+            return;
+        }
+        dfs(s, p + 1, s1, s2, ans);
+        s1.push_back(s[p]);
+        dfs(s, p + 1, s1, s2, ans);
+        s1.pop_back();
+        s2.push_back(s[p]);
+        dfs(s, p + 1, s1, s2, ans);
+        s2.pop_back();
+    }
+
+public:
+    int maxProduct(string s) {
+        int ans = 0;
+        string s1 = "", s2 = "";
+        dfs(s, 0, s1, s2, ans);
+        return ans;
+    }
+};
